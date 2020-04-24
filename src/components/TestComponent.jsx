16bfe989856cce6764f80ecb01cc0244e4363457
Ally Brannon <allison.brannon@gmail.com>
2020-04-24 14:08:32 -0400

    added test component and context from Frank

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import LoginContext from "../context/LoginContext";
+
+export default class TestComponent extends Component {
+  static contextType = LoginContext;
+  render() {
+    return (
+      <LoginContext.Consumer>
+        {(value) => <p>{value.user_id}</p>}
+      </LoginContext.Consumer>
+    );
+  }
+}
