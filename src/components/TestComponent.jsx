02e04d75184a074b69ac993cc23fa4f2e0b87a72
Ally Brannon <allison.brannon@gmail.com>
2020-04-25 13:01:35 -0400

    updating test component and user apps to match franks

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import LoginContext from "../context/LoginContext";
 
-<<<<<<< HEAD
 export default class TestComponent extends Component {
   static contextType = LoginContext;
   render() {
@@ -12,21 +11,3 @@ export default class TestComponent extends Component {
     );
   }
 }
-=======
-export default class TestComponent extends Component{
-
-    static contextType = LoginContext;
-    render() {
-
-        return (
-            <LoginContext.Consumer>
-                {value => (
-                <p>
-                    {value.user_id}
-                </p>
-                )}
-            </LoginContext.Consumer>
-        );
-    }
-}
->>>>>>> dd26e0da328cd82d46caf52c2db2f8ba7a2acc20
--- src/components/UserApps.jsx
+++ src/components/UserApps.jsx
@@ -1,54 +1,55 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class UserApps extends Component {
-    state = {
-      apps: [],
-      user_id: '3'
-    };
-  
-    async getData() {
-      let user_id = this.state.user_id;
-      const response = await fetch(`http://localhost:2000/applications/${user_id}`);
-      const data = await response.json();
-      return data;
-    };
+  state = {
+    apps: [],
+    user_id: "500",
+  };
 
-    async componentDidMount() {
-      const apps = await this.getData();
-  
-      this.setState({
-        apps: apps
-      });
-    }
-  
-    render() {
-        let appsArray = this.state.apps;
-        
-        return (
-            <ul className="UserApp">
-                {appsArray.length > 0 ? (
-                appsArray.map(app => (
-                   <li>
-                    { app.city}
-                    <br/>
-                    { app.position}
-                    <br/>
-                    { app.position_description }
-                    <br/>
-                    { app.application_date } 
-                    <br/>
-                    { app.offer_extended }
-                    <br/>
-                    <br/>
+  async getData() {
+    let user_id = this.state.user_id;
+    const response = await fetch(
+      `http://localhost:2000/applications/${user_id}`
+    );
+    const data = await response.json();
+    return data;
+  }
 
-                    </li>
-                ))
-                ) : (
-                <li>No Data</li>
-                )}
-            </ul>
-        );
-      }
-    }
-  export default UserApps;
+  async componentDidMount() {
+    const apps = await this.getData();
+
+    this.setState({
+      apps: apps,
+    });
+  }
+
+  render() {
+    let appsArray = this.state.apps;
+
+    return (
+      <ul className="UserApp">
+        {appsArray.length > 0 ? (
+          appsArray.map((app) => (
+            <li>
+              {app.city}
+              <br />
+              {app.position}
+              <br />
+              {app.position_description}
+              <br />
+              {app.application_date}
+              <br />
+              {app.offer_extended}
+              <br />
+              <br />
+            </li>
+          ))
+        ) : (
+          <li>No Data</li>
+        )}
+      </ul>
+    );
+  }
+}
+export default UserApps;
