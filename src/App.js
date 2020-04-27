7ca09be6750823bfb3a950e426b7f85cbcbb49e6
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-04-27 10:32:21 -0400

    working on context

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginContext from "./context/LoginContext";
-import TestComponent from "./components/TestComponent";
 import Intro from "./components/Intro";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import UserApps from "./components/UserApps";
-import Interviews from "./components/Interviews";
-import "./App.css";
+import TestComponent from "./components/TestComponent";
 
-/*use context hook*/
+/*use context hook?*/
 function App() {
-  const loggedIn = [{
+  const loggedIn = {
     status: "false",
     user_id: 0,
     admin: ""
-  }];
-
-  return (
-    <LoginContext.Provider value = {loggedIn[0]}>
-      <Router>
-        <Route exact path="/">
-          <Header />
-          <Intro />
-          <Footer />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/applications">
-          <UserApps />
-        </Route>
-      </Router>
-      <TestComponent></TestComponent>
-    </LoginContext.Provider>
-  );
-}
+  };
+    return (
+      <div>
+        <LoginContext.Provider value={loggedIn}>
+        <Router>
+          <Route exact path="/">
+            <Header />
+            <Intro />
+            <Footer />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/applications">
+            <UserApps />
+          </Route>
+        </Router>
+        <TestComponent></TestComponent>
+        </LoginContext.Provider>
+      </div>
+    );
+  }
 
 export default App;
--- src/context/LoginContext.js
+++ src/context/LoginContext.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { Component } from 'react'
 
-const LoginContext = React.createContext();
+const LoginContext = React.createContext()
 
-export default LoginContext;
+class LoginProvider extends Component {
+  // Context state
+  state = {
+    status: "false",
+    user_id: 0,
+    admin: ""
+  }
+
+  // Method to update state
+  setUser = user_id => {
+    this.setState(prevState => ({ user_id }))
+  }
+
+  render() {
+    const { children } = this.props
+    const { user_id } = this.state
+    const { setUser } = this
+
+    return (
+      <LoginContext.Provider
+        value={{
+          user_id,
+          setUser,
+        }}
+      >
+        {children}
+      </LoginContext.Provider>
+    )
+  }
+}
+
+export default LoginContext
+
+export { LoginProvider }
