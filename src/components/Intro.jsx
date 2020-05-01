73ffc32afe54b5109bec033835e6e8c7c4beec72
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-05-01 10:48:42 -0400

    Made some minor changes to the button values in interviews entry and removed styling from the intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,16 +4,10 @@ class Intro extends Component {
   render() {
     return (
       <div>
-        <div style={header1}>Keep track of your applications</div>
+        <div>Keep track of your applications</div>
       </div>
     );
   }
 }
 
-const paragraph = {
-  fontSize: "40px",
-  fontFamily: "Roboto, sans-serif",
-  textAlign: "center",
-  marginTop: "0",
-};
 export default Intro;
